141bfa7d9df98d6ada4e9fa6c55e5c0449668655
andoniVT <andoni.valverde@ucsp.edu.pe>
2015-10-01 14:15:33 -0500

    adding

--- Test/main.py
+++ Test/main.py
@@ -10,13 +10,73 @@ from Test.Utils import write_data_to_disk , load_data_from_disk , expand
 from Test.Classifier import SupervisedClassifier as SC
 from Test.unsupervisedClassifier import Unsupervised
 
+simpleVectorizer = "Models/simpleVectorizer.pk1"
+tfidfModel = "Models/tfidfModel.pk1"
+tfidfVectorizer = "Models/tfidfVectorizer.pk1"
+
+SVM = "Classifiers/SVM.pk1"
+NB = "Classifiers/NB.pk1"
+ME = "Classifiers/ME.pk1"
+DT = "Classifiers/DT.pk1"
+
+
+corpusTrain1 = "Corpus/socialtv-tweets-train-tagged.xml"
+corpusTrain2 = "Corpus/stompol-tweets-train-tagged.xml"
+corpusTest1 = "Corpus/socialtv-tweets-train-tagged.xml"
+
+
 class Manager(object):
     
     def __init__(self):
-        pass 
-    
-    def trainClassifiers(self, corpus_data=""):
         pass
+    
+    def procesar(self, file, type):
+        if type == 1:
+            comentarios = []
+            obj = Reader(file, 1)
+            for i in obj.read():
+                for j in i:
+                    proc = TextCleaner(j[0])
+                    value = [proc.get_processed_comment() , j[2]]
+                    if value[0] != "None!":
+                        comentarios.append(value)
+            return comentarios
+        else:
+            pass
+        
+    
+    def prepareModels(self, xml_file, type):
+        comentarios = self.procesar(xml_file, type)
+        train = []
+        for i in comentarios:
+            train.append(i[0])
+        
+        model = VM(train)
+        vectorModelData = model.prepare_models()
+        modelVectorizer = vectorModelData[0]
+        modelVectorizerTFIDF = vectorModelData[1]
+        modelTFIDF = vectorModelData[2]
+        
+        write_data_to_disk(simpleVectorizer, modelVectorizer)
+        write_data_to_disk(tfidfVectorizer, modelVectorizerTFIDF)
+        write_data_to_disk(tfidfModel, modelTFIDF)
+                
+    def trainClassifiers(self, xml_file, type):
+        self.prepareModels(xml_file, type)
+        comentarios = self.procesar(xml_file, type)
+        data = load_data_from_disk(tfidfModel)
+        data_expanded = []
+        for i in data:
+            vec = expand(i) 
+            data_expanded.append(vec)
+        labels = []
+        for i in comentarios:
+            labels.append(i[1])
+        fileClassifiers = [SVM, NB, ME, DT]
+        for i in range(4):
+            classifier = SC(data_expanded, labels, i+1)
+            fClass = classifier.train()
+            write_data_to_disk(fileClassifiers[i], fClass)
     
     def test(self, comments, type):
         if type == 1:
@@ -54,4 +114,6 @@ if __name__ == '__main__':
     comments = ["es muy bonito" , "no es muy bonito"]
     
     obj = Manager()
-    print obj.test(comments, 5)
+    obj.trainClassifiers(corpusTrain2, 1)
+    
+    
